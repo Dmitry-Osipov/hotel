c2f1163851731694690b6019887eb726685ff381
Dmitry Osipov <dimaosipov00@gmail.com>
2024-01-04 15:04:52 +0300

    Fixed code style

--- task-3/src/task3/AssemblyLine.java
+++ task-3/src/task3/AssemblyLine.java
@@ -15,18 +15,15 @@ public class AssemblyLine implements IAssemblyLine {
     @Override
     public IProduct assembleProduct(IProduct product) {
         System.out.println("Начинаем сборку автомобиля...\n");
+
         parts.add(new BodyLineStep());
         product.installFirstPart(parts.get(0).buildProductPart());
         parts.add(new ChassisLineStep());
         product.installSecondPart(parts.get(1).buildProductPart());
         parts.add(new EngineLineStep());
         product.installThirdPart(parts.get(2).buildProductPart());
-
-        for (ILineStep part : parts) {
-            part.buildProductPart();
-        }
-
         parts.clear();
+
         System.out.println("Сборка автомобиля окончена!\n");
         return product;
     }
