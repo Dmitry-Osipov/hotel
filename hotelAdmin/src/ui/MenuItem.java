5ff23a3abe40bcddea1abffa93b149b1536bb657
Dmitry Osipov <dimaosipov00@gmail.com>
2024-01-17 19:26:12 +0300

    Make MenuItem record

--- hotelAdmin/src/ui/MenuItem.java
+++ hotelAdmin/src/ui/MenuItem.java
@@ -1,20 +1,6 @@
 package ui;
 
-import lombok.Getter;
-import lombok.ToString;
-
-@Getter
-@ToString
-public class MenuItem {
-    private final String title;
-    private final IAction action;
-    private final Menu nextMenu;
-
-    public MenuItem(String title, IAction action, Menu nextMenu) {
-        this.title = title;
-        this.action = action;
-        this.nextMenu = nextMenu;
-    }
+public record MenuItem(String title, IAction action, Menu nextMenu) {
 
     public void doAction() {
         if (action != null) {
--- hotelAdmin/src/ui/Navigator.java
+++ hotelAdmin/src/ui/Navigator.java
@@ -17,7 +17,7 @@ public class Navigator {
     public void printMenu() {
         System.out.println("\nМеню: " + currentMenu.getName());
         for (int i = 0; i < currentMenu.getMenuItems().length; i++) {
-            System.out.println(i+1 + ". " + currentMenu.getMenuItems()[i].getTitle());
+            System.out.println(i+1 + ". " + currentMenu.getMenuItems()[i].title());
         }
     }
 
@@ -28,8 +28,8 @@ public class Navigator {
 
     private Menu getNextMenu(Integer index) {
         if (index >= 0 && index <= currentMenu.getMenuItems().length-1
-                && currentMenu.getMenuItems()[index].getNextMenu() != null) {
-            return currentMenu.getMenuItems()[index].getNextMenu();
+                && currentMenu.getMenuItems()[index].nextMenu() != null) {
+            return currentMenu.getMenuItems()[index].nextMenu();
         }
 
         return rootMenu;
